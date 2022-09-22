1bff1790a9dfd6b0ab140d8cc5f5b122f453935d
Hilario Torres <hilatorres2002@gmail.com>
2022-09-22 00:07:56 -0300

    detailItem2

--- src/components/itemDetail.jsx
+++ src/components/itemDetail.jsx
@@ -2,13 +2,15 @@ import ItemCount from "./itemCount"
 
 const ItemDetail = ({juegos}) => {
     return(
-            <div>
-                <img src={juegos.imagen} className="" alt="imagen Porducto" />
-                    <h3 className="">{juegos.nombre}</h3>
-                    <p>{juegos.descripcion}</p>
-                    <p className= "">Stock {juegos.stock}</p>
-                    <p className="">${juegos.precio}</p>
-                <ItemCount />
+            <div className="gridDetailItems" >
+                <img src={juegos.imagen} className="tamañoImagenDetail" alt="imagen Porducto" />
+                <div className="divPaddingLeft">
+                    <h3 className="tituloItemDetail">{juegos.nombre}</h3>
+                    <p className= "descripcionItemDetail">{juegos.descripcion}</p>
+                    <p className= "stockItemDetail">Stock {juegos.stock}</p>
+                    <p className="precioItemDetail">${juegos.precio}</p>
+                <ItemCount className="añadirCarritoItemDetail"/>
+                </div>
             </div>
     )
 }
